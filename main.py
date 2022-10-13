d1cf3195433114b9cbb17493dffdb2ee361f40c1
Desmondch27 <browndesmond27@gmail.com>
2022-10-13 00:04:50 -0400

    Add FPS Timing And Comment Data Flow
    - Move around some of the data and comment as to how the general game logic will flow
    - Also add FPS Clock for timing the game at the FPS set inside of Settings.py
    - Update the other Settings file to be "Settings.ini".  (No actual use of the file yet, but will help in future when we want to update data without rebuilding the code)

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ window = pygame.display.set_mode(Settings.SCREEN, pygame.RESIZABLE)
 
 pygame.display.set_caption('Pong 2.0 Extreme')
 
+clock = pygame.time.Clock()
+
 
 paddle_left = Paddle(10, window.get_height() / 2)
 paddle_right = Paddle(window.get_width() - 40, window.get_height() /2)
@@ -16,19 +18,27 @@ running = True
 color = pygame.Color(0, 0, 0)
 
 while running:
+    # Wait for one full frame at framerate (also grab delta time -- time since last frame passed) #
+    delta_time: float = clock.tick(Settings.FPS)
+
+    # Handle Pygame Events #
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    window.fill(color)
+    # Handle Input From Players #
+
+    # Update Game Objects (move, collide, etc.) #
     paddle_left.Move(window,pygame.K_w, pygame.K_s)
     paddle_right.Move(window, pygame.K_UP, pygame.K_DOWN)
+    ball.move()
+
+    # Handle Drawing Window #
+    window.fill(color)
     window.blit(paddle_left.paddle, (paddle_left.x, paddle_left.y))
     window.blit(paddle_right.paddle, ((window.get_width() - paddle_right.width) - 10, paddle_right.y))
     window.blit(ball, (ball.x, ball.y))
 
-    ball.move()
-    print(ball.x, ball.y)
-
+    # Fully update screen with changes #
     pygame.display.flip()
 
