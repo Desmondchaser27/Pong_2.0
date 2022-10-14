14162e02bd6de902f3e800404967ea08ae5e23ca
Desmondch27 <browndesmond27@gmail.com>
2022-10-13 22:37:51 -0400

    Add FileReader and Read Settings File Capability
    - Added a basic filereader py file.  Currently, just reads settings file.
    - The settings.py file now has a function to take data from the settings dictionary created from parsing the settings file and applies the data to the settings variables
    
    So now, we can implement various settings under the Settings.ini file and have them implemented into the game at boot up without having to technically recompile code.

---
+++ Helpers/FileReader.py
@@ -0,0 +1,38 @@
+import io
+import typing
+from typing import Union
+from enum import Enum
+
+
+class SettingsType(Enum):
+    NULL = 0
+    SKIP = 1
+    DISPLAY = 2
+
+
+def read_settings_file(filepath: str) -> dict[SettingsType, dict[str, Union[int, float, str]]]:
+    file: typing.TextIO = open(filepath, 'r')
+    line: str = "_"  # Filler to get in while loop
+    settings_type: SettingsType = SettingsType.SKIP
+    dictionary: dict[SettingsType, dict[str, Union[int, float, str]]] = dict()
+    while not line == '':
+        line = file.readline().strip()
+
+        # TODO: as we add more setting options expand this match/case
+        match line:
+            case "[Display]":
+                settings_type = SettingsType.DISPLAY
+
+        while "[" not in line and "]" not in line and not line == '':
+            words: list[str] = line.split('=')
+            for i in range(len(words)):
+                words[i] = words[i].strip()
+            if len(words) == 2:
+                entry = dictionary.get(settings_type)
+                if entry is None:
+                    dictionary[settings_type] = {words[0]: words[1]}
+                else:
+                    entry[words[0]] = words[1]
+            line = file.readline().strip()
+    file.close()
+    return dictionary
